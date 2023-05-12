22223951e5bae867b87b4c96128ab6a5ac9085ea
alaasairafi7 <129796008+alaasairafi7@users.noreply.github.com>
2023-05-12 04:17:58 +0300

    Delete Menu.java

--- Menu.java
+++
@@ -1,9 +0,0 @@
-package ALRAHMA1.company;
-
-public abstract class Menu {
-
-    public abstract void description();
-    public abstract void BreakFast();
-    public abstract void lunch();
-    public abstract void Dinner();
-}
